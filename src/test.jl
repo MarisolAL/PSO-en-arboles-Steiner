04c2ccb39234eadf3f790a884623aafdadee8a21
Unknown <marisolamezcua@ciencias.unam.mx>
2018-11-08 15:41:09 -0600

    Modulo con pruebas de particulas terminado

---
+++ src/test.jl
@@ -0,0 +1,27 @@
+module test
+include("particula.jl")
+"Ejemplos de prueba"
+function suma(x)
+    return sum(x)
+end
+a = particula.Particula(4,[1,2,6,9],suma)
+println(a)
+a.posicion = a.posicion + a.posicion
+particula.actualiza_mejor_individual(a)
+println("Cambio")
+println(a)#Si cambia los fitness
+a.posicion = [0,0,0,0]
+particula.actualiza_mejor_individual(a)
+println("Cambio")
+println(a)#Si actualiza los mejores ss
+a.empeora = 50
+particula.debo_matar(a,10)
+println(a)
+println("Cambiando velocidad..")
+mejor = particula.Particula(4,[1,1,1,1],suma)
+particula.actualiza_velocidad(a,mejor)
+println(a.velocidad)
+println("Cambiando posicion..")
+particula.actualiza_posicion(a)
+println(a.posicion)
+end
