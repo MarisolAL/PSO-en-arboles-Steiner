83098e368b1b3e1a641ef8c273e388dfa5a001a6
Unknown <marisolamezcua@ciencias.unam.mx>
2018-11-21 19:06:51 -0600

    Avance en Steiner
    Cambiar a usar SImpleWeightedGraphs

--- src/steiner.jl
+++ src/steiner.jl
@@ -3,13 +3,48 @@ include("particula.jl")
 include("pso.jl")
 using Graphs
 
-function construye_arbol(S)
+"Funcion que calcula la distancia entre 2 arreglos bidimensionales"
+function distancia_2_puntos(punto1::Array{Float64}, punto2::Array{Float64})
+    dist = sqrt((punto1[1] - punto2[1])^2 + ((punto1[2] - punto2[2])^2))
+    return dist
+end
 
+"Funcion que calcula la longitud de un conjunto"
+function size_set(S)
+    long = 0
+    for i in S
+        long +=1
+    end
+    return long
+end
+
+"Funcion que contruye el arbol dado el conjunto de puntos obtenidos"
+function construye_arbol(S)
+    S = map(x -> float(x),S) #Pasamos todos los elementos a flotantes
+    long = size_set(S)
+    vertices = Array{ExVertex}(long,1)
+    indice = 1
+    for i in S
+        vertices[indice] = ExVertex(indice, string("vertice ",indice))
+        vertices[indice].attributes["punto"] = i
+        indice += 1
+    end #En este punto tenemos la lista de los vertices
+    println(vertices)
+    aristas = Set([])
+    for i in vertices
+        for j in vertices
+            if i != j
+                push!(aristas, [i, j])
+            end
+        end
+    end
+    println("################")
+    println(aristas)
 end
 
 
 function obten_peso_total(arbol)
-
+return 0
 end
 
 mutable struct Steiner
@@ -25,4 +60,8 @@ mutable struct Steiner
     end
 end
 
+S = Set([[1,0],[3,0],[0,7],[3,4],[2,7]])
+S = map(x -> float(x),S)
+println(size_set(S))
+construye_arbol(S)
 end
