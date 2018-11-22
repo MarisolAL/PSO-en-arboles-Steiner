4636f2088a6f334dabbbc51f2f0150cfc85bbefc
Unknown <marisolamezcua@ciencias.unam.mx>
2018-11-21 19:06:06 -0600

    Pruebas terminadas hasta el modulo de PSO

--- src/test.jl
+++ src/test.jl
@@ -5,13 +5,13 @@ include("pso.jl")
 function suma(x)
     return sum(x)
 end
-a = particula.Particula(4,[1,2,6,9],suma)
+a = particula.Particula(2,[1,2],suma)
 println(a)
 a.posicion = a.posicion + a.posicion
 particula.actualiza_mejor_individual(a)
 println("Cambio")
 println(a)#Si cambia los fitness
-a.posicion = [0,0,0,0]
+a.posicion = [0,0]
 particula.actualiza_mejor_individual(a)
 println("Cambio")
 println(a)#Si actualiza los mejores ss
@@ -19,14 +19,14 @@ a.empeora = 50
 particula.debo_matar(a,10)
 println(a)
 println("Cambiando velocidad..")
-mejor = particula.Particula(4,[1,1,1,1],suma)
+mejor = particula.Particula(2,[1,1],suma)
 particula.actualiza_velocidad(a,mejor)
 println(a.velocidad)
 println("Cambiando posicion..")
 particula.actualiza_posicion(a)
 println(a.posicion)
 ###PSO
-p = pso.PSO(suma,10,[0.0,0.0])
-println(p)
-println(pso.corre_pso(30, p))
+p = pso.PSO(suma,10,[5.0,0.0])
+#println(p)
+println(pso.corre_pso(10000, p))
 end
